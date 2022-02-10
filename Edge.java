bf0c71613b4806a826c12c82b7354b34b20bb253
urmi2394 <urmi2394@gmail.com>
2022-02-10 19:10:05 +0000

    Selenium Homework

--- Edge.java
+++ Edge.java
@@ -1,14 +1,15 @@
 package seleniumsession;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 
-public class EdgeDriver {
+public class Edge {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.edge.driver","C:\\Users\\baps\\IdeaProjects\\Edge\\msedgedriver.exe");
 
-        WebDriver driver = new org.openqa.selenium.edge.EdgeDriver();
+        WebDriver driver = new EdgeDriver();
         Thread.sleep(3000);
         driver.quit();
 
